6cddb0dcd787a59d4b86417a2f9ba4a6364c20bb
Едис Сюлейман <es65949232@edu.mon.bg>
2020-05-29 03:50:41 +0300

    first commit

---
+++ Data/Models/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Employee
+    {
+		[Key]
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string firstName;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; }
+		}
+
+		private string username;
+
+		public string Username
+		{
+			get { return username; }
+			set { username = value; }
+		}
+
+		private string password;
+
+		public string Password
+		{
+			get { return password; }
+			set { password = value; }
+		}
+
+		private int storage_id;
+
+		public int Storage_Id
+		{
+			get { return storage_id; }
+			set { storage_id = value; }
+		}
+
+	}
+}
