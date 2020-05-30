483ba5be2b73bc5e29fd24118c1f038bf6ec2a3a
Едис Сюлейман <es65949232@edu.mon.bg>
2020-05-30 04:03:49 +0300

    Business Logic Revamped

---
+++ Business/EmployeeBusiness.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Business
+{
+    class EmployeeBusiness
+    {
+        public staff GetEmployee(int id)
+        {
+            using (var staffContext = new storage_managementEntities())
+            {
+                foreach (var employee in staffContext.staffs)
+                {
+                    if (employee.id_staff.Equals(id))
+                    {
+                        return employee;
+                    }
+                }
+                staffContext.SaveChanges();
+                return null;
+            }
+        }
+
+        //Ако върне тру значи служителя е премахнат успешно
+        public bool DeleteEmployee(string param)
+        {
+            using (var staffContext = new storage_managementEntities())
+            {
+                int id;
+                if (int.TryParse(param, out id) == true)
+                {
+                    if (DeleteEmployeeById(id) == true)
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+
+                }
+                else
+                {
+                    if (DeleteEmployeeByName(param) == true)
+                        return true;
+                    else
+                        return false;
+
+                }
+            }
+        }
+        //Вътрешни методи
+        private bool DeleteEmployeeByName(string username)
+        {
+            using (var staffContext = new storage_managementEntities())
+            {
+                foreach (var employee in staffContext.staffs)
+                {
+                    if (employee.username.Equals(username))
+                    {
+                        staffContext.staffs.Remove(employee);
+                        staffContext.SaveChanges();
+                        return true;
+                    }
+
+                }
+                return false;
+            }
+        }
+
+        private bool DeleteEmployeeById(int id)
+        {
+            using (var staffContext = new storage_managementEntities())
+            {
+                foreach (var employee in staffContext.staffs)
+                {
+                    if (employee.id_staff.Equals(id))
+                    {
+                        staffContext.staffs.Remove(employee);
+                        staffContext.SaveChanges();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Business/ProductBusiness.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Business
+{
+    public class ProductBusiness
+    {
+        public bool CreateProduct(string name, string brand, double price, int quantity, bool isAvailable, int storageId)
+        {
+            using (var productContext = new storage_managementEntities())
+            {
+                foreach (var product in productContext.items)
+                {
+                    if (product.name.Equals(name))
+                    {
+                        return false;
+                    }
+                }
+                item item = new item(name, brand, price, quantity, isAvailable, storageId);
+                productContext.items.Add(item);
+                productContext.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool DeleteProduct(string name)
+        {
+            int id;
+            if (int.TryParse(name, out id))
+            {
+                if (DeleteProductById(id))
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                if (DeleteProductByName(name))
+                    return true;
+                else
+                    return false;
+            }
+        }
+        private bool DeleteProductById(int id)
+        {
+            using (var productContext = new storage_managementEntities())
+            {
+                foreach (var product in productContext.items)
+                {
+                    if (product.id_item.Equals(id))
+                    {
+                        productContext.items.Remove(product);
+                        productContext.SaveChanges();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        private bool DeleteProductByName(string name)
+        {
+            using (var productContext = new storage_managementEntities())
+            {
+                foreach (var product in productContext.items)
+                {
+                    if (product.name.Equals(name))
+                    {
+                        productContext.items.Remove(product);
+                        productContext.SaveChanges();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public item GetProduct(string param)
+        {
+            int id;
+            if (int.TryParse(param, out id))
+            {
+                return GetProductById(id);
+            }
+            else
+            {
+                return GetProductByName(param);
+            }
+        }
+        private item GetProductById(int id)
+        {
+            using (var productContext = new storage_managementEntities())
+            {
+                foreach (var product in productContext.items)
+                {
+                    if (product.id_item.Equals(id))
+                    {
+                        return product;
+                    }
+                }
+                return null;
+            }
+        }
+        private item GetProductByName(string name)
+        {
+            using (var productContext = new storage_managementEntities())
+            {
+                foreach (var product in productContext.items)
+                {
+                    if (product.name.Equals(name))
+                    {
+                        return product;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
